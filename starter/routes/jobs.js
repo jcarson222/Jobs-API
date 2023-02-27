d51b12028e432964e22f204f8df8a78fbb680b31
Jesse Carson <jmcarson222@gmail.com>
2023-02-27 10:44:27 -0600

    User-Schema

--- starter/routes/jobs.js
+++ starter/routes/jobs.js
@@ -11,13 +11,13 @@ const {
 
 router.route("/").post(createJob).get(getAllJobs);
 
-router.route("/:id").get(getJob).patch(createJob).delete(deleteJob);
+router.route("/:id").get(getJob).patch(updateJob).delete(deleteJob);
 
 // OR
 // router.post('/', createJob);
 // router.get('/', getAllJobs);
 // router.get("/:id", getJob);
-// router.patch("/:id", createJob);
+// router.patch("/:id", updateJob);
 // router.delete("/:id", deleteJob);
 
 module.exports = router;
