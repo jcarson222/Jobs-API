e06c50258ac22cfe027ab143553cef07ede7be5d
Jesse Carson <jmcarson222@gmail.com>
2023-03-02 14:43:02 -0600

    customError

--- starter/middleware/error-handler.js
+++ starter/middleware/error-handler.js
@@ -1,10 +1,26 @@
-const { CustomAPIError } = require('../errors')
-const { StatusCodes } = require('http-status-codes')
+const { CustomAPIError } = require("../errors");
+const { StatusCodes } = require("http-status-codes");
 const errorHandlerMiddleware = (err, req, res, next) => {
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message })
-  }
-  return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
-}
+  let customError = {
+    statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
+    msg: err.message || "Something went wrong",
+  };
 
-module.exports = errorHandlerMiddleware
+  if (err instanceof CustomAPIError) {
+    return res.status(err.statusCode).json({ msg: err.message });
+  }
+
+  // This if statement takes the mongoose error for duplicate emails - custom msg and an accurate status code
+  if (err.code && err.code === 11000) {
+    customError.msg = `Duplicate value entered for ${Object.keys(
+      err.keyValue
+    )} field, please enter a different value`;
+
+    customError.StatusCode = 400;
+  }
+
+  //return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ err })
+  return res.status(customError.statusCode).json({ msg: customError.msg });
+};
+
+module.exports = errorHandlerMiddleware;
